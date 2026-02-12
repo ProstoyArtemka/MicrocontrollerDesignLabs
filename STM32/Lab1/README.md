9d1c509c73bf360d28fb18ed0df985099dc03140
ProstoyArtemka <whitezunder@yandex.ru>
2026-02-12 22:55:08 +0700

    Init, first lab done

---
+++ STM32/Lab1/README.md
@@ -0,0 +1,9 @@
+<p style="text-align: center; font-size: 32px">Лабораторная №1 по Проектированию микроконтроллеров</p>
+
+<br>
+<br>
+<br>
+
+<p style="text-align: center; font-size: 24px">Вычислить выражение на языке ASM для STM32F407VG</p>
+
+$$R0 = \sum_{i=1}^{5} \frac{R1^3}{R2 + 4 * i}$$
---
+++ STM32/Lab1/main.s
@@ -0,0 +1,39 @@
+	AREA		SRAM1,		NOINIT,		READWRITE
+	SPACE		0x0200
+Stack_Top
+
+	AREA		RESET,		DATA,		READONLY
+	DCD		Stack_Top					;[0x0000-0x0003]
+	DCD		Start						;[0x0004-0x0007]
+
+	AREA		PROGRAM,	CODE,		READONLY
+	ENTRY
+	
+Start
+	
+	MOV r1, #5 ; R1 = ?
+	MOV r2, #10 ; R2 = ?
+	MOV r3, #6 ; R3 = Value i = 5 + 1
+	MOV r4, #4 ; R4 = Value 4 for multiplying
+	
+	
+	
+	MUL r5, r1, r1 ; R5 = R1 * R1
+	MUL r5, r1, r5 ; R5 = R1 * R1 * r1
+
+Cycle
+
+	SUBS r3, #1 ; Decrement by 1
+
+	MUL r6, r3, r4 ; R6 = i * 4
+	ADD r6, r2 ; R6 = (i * 4) + R2
+	
+	UDIV r7, r5, r6
+	
+	ADD r0, r7
+
+	BNE Cycle
+
+	NOP
+
+	END
